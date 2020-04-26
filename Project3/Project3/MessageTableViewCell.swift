bdbe4386d6865e54af2d8a395ca967219cb03c77
user167012 <user167012@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-04-26 00:28:11 -0400

    remake message project

---
+++ Project3/Project3/MessageTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MessageTableViewCell.swift
+//  Project3
+//
+//  Created by user167012 on 4/25/20.
+//  Copyright © 2020 user167012. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+    @IBOutlet weak var picView: UIImageView!
+    
+    @IBOutlet weak var textView: UILabel!
+}
---
+++ Project3/Project3/MessageViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MessageViewController.swift
+//  Project3
+//
+//  Created by user167012 on 4/25/20.
+//  Copyright © 2020 user167012. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "imageCell"
+
+class MessageViewController: UIViewController, UITableViewDataSource, UITextFieldDelegate {
+    var messageURLs: [String] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageURLs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MessageTableViewCell
+        
+            // Configure the cell
+            //cell.picView.download(imageURLs[indexPath.row])
+        
+            return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
